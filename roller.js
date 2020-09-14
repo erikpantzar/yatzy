cff5741d57bcd45c665b9d9eb210d0a6d5b4f3db
Erik Pantzar <erik@epantzar.se>
2020-09-14 11:17:54 +0200

    Save dice

--- roller.js
+++ roller.js
@@ -11,14 +11,23 @@ const rollDice = () => {
   const ROLLING_ANIMATOIN = 700
   const ROLLING_ANIMATION_CLASSNAME = 'rolling'
 
-  dices.forEach((dice) => dice.classList.add(ROLLING_ANIMATION_CLASSNAME))
+  dices.forEach((dice) => {
+    // check if dice is not checked
+    if (!dice.querySelector('input').checked) {
+      dice.classList.add(ROLLING_ANIMATION_CLASSNAME)
+    } else {
+      return false
+    }
+  })
 
   setTimeout(() => {
     dices.forEach((dice) => {
       // stop animation
-      dice.classList.remove(ROLLING_ANIMATION_CLASSNAME)
-      // set the value to each dice element
-      setValueToDice(dice)
+      if (dice.classList.contains(ROLLING_ANIMATION_CLASSNAME)) {
+        dice.classList.remove(ROLLING_ANIMATION_CLASSNAME)
+        // set the value to each dice element
+        setValueToDice(dice)
+      }
     })
   }, ROLLING_ANIMATOIN)
 }
@@ -27,3 +36,6 @@ const setValueToDice = (diceElement) => {
   const randomDiceValue = () => Math.round(Math.random() * (6 - 1) + 1)
   diceElement.dataset.value = randomDiceValue()
 }
+
+// initial roll
+rollDice()
