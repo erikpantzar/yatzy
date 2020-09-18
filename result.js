288ffe9550c7aa386d63ca14e11ad31aa23f08c8
Erik Pantzar <erik@epantzar.se>
2020-09-18 20:01:01 +0200

    Results are working

--- result.js
+++ result.js
@@ -17,15 +17,45 @@ const handleResult = (event) => {
         break
       case 'bonus':
         targetEl.value = getBonus(targetEl) ? 50 : 0
+        break
       case 'pair':
-        targetEl.value = getPair(targetEl)
+        targetEl.value = getPair()
         break
       case 'twopair':
-        targetEl.value = getTwoPair(targetEl)
+        targetEl.value = getTwoPair()
+        break
+      case 'three':
+        targetEl.value = threeOfAKind()
+        break
+      case 'four':
+        targetEl.value = fourOfAkind()
+        break
+      case 'fullhouse':
+        targetEl.value = fullHouse()
+        break
+      case 'lowstraight':
+        targetEl.value = loLadder()
+        break
+      case 'histraight':
+        targetEl.value = hiLadder()
+        break
+      case 'chance':
+        targetEl.value = chance()
+        break
+      case 'yatzy':
+        targetEl.value = yatzee()
+        break
+      case 'total':
+        targetEl.value = getTotal(targetEl)
+        break
       default:
         return false
     }
   }
+
+  // unselect dice
+  const diceCheckboxes = document.querySelectorAll('.dice-container input')
+  diceCheckboxes.forEach((check) => (check.checked = false))
 }
 
 // Returns score of active dice into given target number
@@ -55,15 +85,29 @@ const getBonus = (el) => {
   return partSum >= 63
 }
 
+const getTotal = (el) => {
+  const scores = [sum(el), getBonus(el) ? 50 : 0]
+
+  document
+    .querySelectorAll(
+      '[data-target="pair"], [data-target="three"], [data-target="four"], [data-target="twopair"], [data-target="yatzy"], [data-target="chance"], [data-target="histraight"], [data-target="lowstraight"], [data-target="fullhouse"]'
+    )
+    .forEach((element) => {
+      scores.push(Number(element.value))
+    })
+
+  return scores.reduce((a, b) => a + b, 0)
+}
+
 // default to two for a pair
-const getTheSame = (array, amount = 2) => {
-  let result = 0
+const getTheSame = (array, amount = 2, not = null) => {
+  let result = []
 
   const getSame = (array, index = 0) => {
-    const same = array.filter((n) => n === array[index])
+    const same = array.filter((n) => n === array[index] && n !== not)
 
     if (same.length >= amount) {
-      result = same[0] * amount
+      result = new Array(amount).fill(same[0])
     } else {
       if (index !== array.length) {
         getSame(array, index + 1)
@@ -78,17 +122,112 @@ const getTheSame = (array, amount = 2) => {
 const getPair = () => {
   // hi to low
   const dices = diceToArray().sort((a, b) => b - a)
-  return getTheSame(dices, 2)
+
+  const pair = getTheSame(dices)
+  if (pair.length === 2) {
+    return pair.reduce((a, b) => a + b, 0)
+  }
+
+  return 0
 }
 
 const getTwoPair = () => {
-  // const dices = diceToArray()
-  const dices = [2, 4, 5, 3, 3, 3, 3].sort((a, b) => b - a)
-  console.log(getTheSame(dices, 2))
+  let result = 0
+  const dices = diceToArray()
 
-  return 666
+  const first = getTheSame(dices, 2)
+  const second = getTheSame(dices, 2, first[0])
+
+  if (first.length === 2 && second.length === 2) {
+    result = [...first, ...second].reduce((a, b) => a + b, 0)
+  }
+
+  return result
 }
 
-getTwoPair()
+const threeOfAKind = () => {
+  const dices = diceToArray()
+  const first = getTheSame(dices, 3)
+
+  if (first.length === 3) {
+    return first.reduce((a, b) => a + b, 0)
+  } else {
+    return 0
+  }
+}
+
+const fourOfAkind = () => {
+  const dices = diceToArray()
+
+  const first = getTheSame(dices, 4)
+  if (first.length === 4) {
+    return first.reduce((a, b) => a + b, 0)
+  } else {
+    return 0
+  }
+}
+
+const yatzee = () => {
+  const dices = diceToArray()
+
+  const first = getTheSame(dices, 5)
+  if (first.length === 5) {
+    return 50
+  } else {
+    return 0
+  }
+}
+
+const hiLadder = () => {
+  const dices = diceToArray().sort((a, b) => b - a)
+
+  if (
+    dices[0] === 6 &&
+    dices[1] === 5 &&
+    dices[2] === 4 &&
+    dices[3] === 3 &&
+    dices[4] === 2
+  ) {
+    return 20
+  }
+
+  return 0
+}
+
+const loLadder = () => {
+  const dices = diceToArray().sort((a, b) => b - a)
+
+  if (
+    dices[0] === 5 &&
+    dices[1] === 4 &&
+    dices[2] === 3 &&
+    dices[3] === 2 &&
+    dices[4] === 1
+  ) {
+    return 15
+  }
+
+  return 0
+}
+
+const fullHouse = () => {
+  let result = 0
+  const dices = diceToArray().sort((a, b) => b - a)
+
+  const three = getTheSame(dices, 3)
+  if (three.length === 3) {
+    const two = getTheSame(dices, 2, three[0])
+
+    if (two.length === 2) {
+      result = [...three, ...two].reduce((a, b) => a + b, 0)
+    }
+  }
+
+  return result
+}
+
+const chance = () => {
+  return diceToArray().reduce((a, b) => a + b, 0)
+}
 
 document.getElementById('jsYatzyTable').addEventListener('click', handleResult)
--- same.js
+++ same.js
@@ -1,12 +1,12 @@
 // default to two for a pair
-const getTheSame = (array, amount = 2) => {
-  let result = 0
+const getTheSame = (array, amount = 2, not = null) => {
+  let result = []
 
   const getSame = (array, index = 0) => {
-    const same = array.filter((n) => n === array[index])
+    const same = array.filter((n) => n === array[index] && n !== not)
 
     if (same.length >= amount) {
-      result = same[0] * amount
+      result = new Array(amount).fill(same[0])
     } else {
       if (index !== array.length) {
         getSame(array, index + 1)
@@ -24,20 +24,28 @@ if (false) {
     getTheSame(
       [1, 2, 3, 4, 6, 9].sort((a, b) => b - a),
       2
-    ) === 0
+    )
   )
 
   console.log(
     getTheSame(
       [1, 2, 3, 4, 2, 9, 9, 9].sort((a, b) => b - a),
       2
-    ) === 18
+    )
   )
 
   console.log(
     getTheSame(
       [1, 2, 3, 4, 2, 9, 9, 9].sort((a, b) => b - a),
       3
-    ) === 27
+    )
+  )
+
+  console.log(
+    getTheSame(
+      [4, 4, 2, 2].sort((a, b) => b - a),
+      2,
+      4
+    )
   )
 }
