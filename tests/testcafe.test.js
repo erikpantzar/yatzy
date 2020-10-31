590c08c3a6bef567ac9fee702cb6db061592cf76
Erik Pantzar <erik@epantzar.se>
2020-10-31 12:13:46 +0100

    Added server to host app (#5)
    * Added server to host app
    * Remove unused action

--- tests/testcafe.test.js
+++ tests/testcafe.test.js
@@ -1,10 +1,9 @@
-import { Selector } from 'testcafe';
+import { Selector } from 'testcafe'
 
-fixture `Getting Started`
-    .page `http://epantzar.se/yatzy-caddee/`;
+fixture`Getting Started`.page`http://127.0.0.1:8080/`
 
-test('My first test', async t => {  
-      const string = 'Roll'
-      // Obtain the text of the article header
-      await t.expect(Selector('.button-roller').innerText).eql(string);
-});
+test('My first test', async (t) => {
+  const string = 'Roll'
+  // Obtain the text of the article header
+  await t.expect(Selector('.button-roller').innerText).eql(string)
+})
