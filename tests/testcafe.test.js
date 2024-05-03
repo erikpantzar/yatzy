2062f0e6ca348f7655d88bd8499e58e95899b472
Erik Pantzar <erik@epantzar.se>
2024-05-03 17:27:00 +0200

    remove tests, increase contrast on darkmode, added checks for navigating to not clear results

--- tests/testcafe.test.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe'
-
-fixture`Getting Started`.page`http://127.0.0.1:8080/`
-
-test('My first test', async (t) => {
-  const string = 'Roll'
-  // Obtain the text of the article header
-  await t.expect(Selector('.button-roller').innerText).eql(string)
-})
